1a12ff09fd8e76a423ef294ed99c7c85c6c08c99
AnviAlm <anvi18hakus@gmail.com>
2024-01-07 22:53:21 -0500

    editando

--- calculadoraPromedio.js
+++ calculadoraPromedio.js
@@ -30,6 +30,18 @@ en clase. El promedio obtenido es [indicar promedio].'
 
 let nombreAlumno= prompt('Ingrese el nombre del alumno')
 
-let nombreMateria= prompt('Ingrese el nombre de la materia')
+let Materia= prompt('Ingrese el nombre de la materia')
 
+let nota1 = parseInt(prompt("Ingrese la primer nota de 0 a 10:"));
+let nota2 = parseInt(prompt("Ingrese la segunda nota de 0 a 10:"));
+let nota3 = parseInt(prompt("Ingrese la tercera nota de 0 a 10:"));
+
+if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10) {
+    console.log ("Por favor ingrese una nota valida en el rango de 0 a 10")
+} else { 
+    console.log ("Datos validos")
+}
+
+
+    let promedio
 
