d6edb207507ad8f386073fdf751cd372e2b5121a
AnviAlm <anvi18hakus@gmail.com>
2024-01-07 23:30:30 -0500

    Ejer2logproterm

--- calculadoraPromedio.js
+++ calculadoraPromedio.js
@@ -35,13 +35,23 @@ let Materia= prompt('Ingrese el nombre de la materia')
 let nota1 = parseInt(prompt("Ingrese la primer nota de 0 a 10:"));
 let nota2 = parseInt(prompt("Ingrese la segunda nota de 0 a 10:"));
 let nota3 = parseInt(prompt("Ingrese la tercera nota de 0 a 10:"));
+/*
+if (nota1 >= 0 && nota1 <= 10 && nota2 >= 0 && nota2 <= 10 && nota3 >= 0 && nota3 <= 10) {
+  var promedio = (nota1 + nota2 + nota3) / 3;
+*/
 
 if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10) {
     console.log ("Por favor ingrese una nota valida en el rango de 0 a 10")
 } else { 
-    console.log ("Datos validos")
+    console.log (nota1)
+    console.log (nota2)
+    console.log (nota3)
+    let promedio=(nota1+nota2+nota3) /3
+    console.log(promedio)
+
+    if (promedio >= 7) {
+        alert(`Felicidades, ${nombreAlumno},la materia de ${Materia}, la aprobaste con un promedio de ${promedio}.`);
+      } else {
+        alert(nombreAlumno + `, Gracias por tu esfuerzo. Tu promedio en la materia de ${Materia} es ` + promedio.toFixed(2) + " Puntaje insuficiente para aprobar...  Nos vemos pronto en clase");
+      }
 }
-
-
-    let promedio
-
