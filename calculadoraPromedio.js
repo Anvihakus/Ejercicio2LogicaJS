312e360ed4203534cb5c4bc53135046a572ca469
AnviAlm <anvi18hakus@gmail.com>
2024-01-07 22:05:56 -0500

    conpag

---
+++ calculadoraPromedio.js
@@ -0,0 +1,35 @@
+/*
+Notas del ejer 2: Utiliza JavaScript para realizar las operaciones necesarias para calcular 
+el promedio de notas, verificar el rango válido de las notas y mostrar el mensaje 
+correspondiente según el resultado, vamos a tomar en cuenta las instrucciones 
+del ejercicio 1, del cual se realizó diagrama de flujo.
+Desarrolla un programa que calcule el promedio de tres notas ingresadas 
+por el usuario, solicitando también el nombre del alumno. Además, el 
+programa debe verificar si las notas ingresadas se encuentran dentro del 
+rango válido (por ejemplo, de 0 a 10). Una vez calculado el promedio de las 
+tres notas, el programa debe evaluar si dicho promedio es mayor o igual a 7.
+Si el promedio es mayor o igual a 7, el programa imprimirá: '[Nombre del 
+alumno], ¡felicidades! Has aprobado con un promedio de [indicar 
+promedio].' En caso de que el promedio sea menor a 7, el programa 
+imprimirá: '[Nombre del alumno], gracias por tu esfuerzo. Nos vemos 
+pronto en clase. El promedio obtenido es [indicar promedio]. 
+
+Notas del ejer 1: Desarrolla un programa que calcule el promedio de tres notas ingresadas 
+por el usuario, solicitando también el nombre del alumno y la materia. 
+Además, el programa debe verificar si las notas ingresadas se encuentran 
+dentro del rango válido (por ejemplo, de 0 a 10). Una vez calculado el 
+promedio de las tres notas, el programa debe evaluar si dicho promedio es 
+mayor o igual a 7.
+Si el promedio es mayor o igual a 7, el programa imprimirá: 'Nombre del 
+alumno, ¡felicidades! Has aprobado con un promedio de [indicar 
+promedio].' En caso de que el promedio sea menor a 7, el programa 
+imprimirá: 'Nombre del alumno, gracias por tu esfuerzo. Nos vemos pronto 
+en clase. El promedio obtenido es [indicar promedio].'
+*/
+
+
+let nombreAlumno= prompt('Ingrese el nombre del alumno')
+
+let nombreMateria= prompt('Ingrese el nombre de la materia')
+
+
